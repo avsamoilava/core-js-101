76b50c96a01a056312ba0d3e61a932a9932f0deb
Alexandra Samoilova <avsamoilava@gmail.com>
2022-11-25 20:58:15 +0300

    feat: add sortDigitNamesByNumericOrder

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -324,8 +324,11 @@ function getPositivesCount(arr) {
  *   [ 'nine','eight','nine','eight'] => [ 'eight','eight','nine','nine']
  *   [ 'one','one','one','zero' ]     => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  const eq = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  return arr.map((el) => ({ dig: eq.indexOf(el), str: el }))
+    .sort((a, b) => a.dig - b.dig)
+    .map((el) => el.str);
 }
 
 /**
