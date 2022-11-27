52420200cf757c706e9ec42937869ceeaf629904
Alexandra Samoilova <avsamoilava@gmail.com>
2022-11-28 00:34:58 +0300

    feat: add timeSpanToString

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -73,10 +73,16 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const diff = endDate - startDate;
+  const hours = Math.floor(diff / 1000 / 3600);
+  const minutes = Math.floor((diff - hours * 1000 * 3600) / 1000 / 60);
+  const seconds = Math.floor((diff - hours * 1000 * 3600 - minutes * 60 * 1000) / 1000);
+  const ms = diff - seconds * 1000 - minutes * 1000 * 60 - hours * 1000 * 3600;
+  return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}.${ms.toString().padStart(3, '0')}`;
 }
 
+// console.log(timeSpanToString(new Date(2000,1,1,10,0,0),  new Date(2000, 1, 1, 10, 30, 0)))
 
 /**
  * Returns the angle (in radians) between the hands of an analog clock
