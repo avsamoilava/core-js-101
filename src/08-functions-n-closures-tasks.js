1f299cc9a82b0c938e1a03fd78e45c9688f29362
Alexandra Samoilova <avsamoilava@gmail.com>
2023-01-02 22:36:48 +0300

    feat: add memoize, logger, partialUsingArguments, getIdGeneratorFunction

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -92,8 +92,16 @@ function getPolynom(...args) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const cash = {};
+  return (...args) => {
+    const id = args.map((elem) => `${elem.toString()}:${typeof elem}`).join('|');
+    const value = cash[id];
+    if (value) return value;
+    const res = func(...args);
+    cash[id] = res;
+    return res;
+  };
 }
 
 
@@ -148,8 +156,14 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return (...args) => {
+    const str = JSON.stringify(args);
+    logFunc(`${func.name}(${str.slice(1, str.length - 1)}) starts`);
+    const value = func(...args);
+    logFunc(`${func.name}(${str.slice(1, str.length - 1)}) ends`);
+    return value;
+  };
 }
 
 
@@ -166,8 +180,8 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return (...args2) => fn(...[...args1, ...args2]);
 }
 
 
@@ -188,8 +202,13 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let count = startFrom;
+  return () => {
+    const res = count;
+    count += 1;
+    return res;
+  };
 }
 
 
